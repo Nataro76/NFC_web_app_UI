d02a522702e8bae1e8d5f9341c05e63e7be307f1
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-10-05 10:15:38 +0200

    Update index.js

--- modules/home/index.js
+++ modules/home/index.js
@@ -67,13 +67,15 @@ define([ 'require','libbf'], function ( require, libbf ) {
 
         $ctrl.$onInit = function () {
             $scope.$watch($ctrl.temp,function(){
+                if($ctrl.temp!='undefined'){
                 if($ctrl.temp.length===2){
                     commitAssoc($ctrl.tag,$ctrl.beacon);
                 }
+            }
             })
             $ctrl.ChromSamplesInit();
             window.addEventListener('error', errorFun());
-            console.log('Beta version 1.64/network testing');
+            console.log('Beta version 1.65/network testing');
             try{
                 BFauth.authenticate('admin','D3fAulT-P4ssW0rD',null,'https://beta.orisun-iot.com/');
         
