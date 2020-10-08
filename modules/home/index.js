ccef4b21834c607196cc2f3b179cbb893ae1e84a
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-10-08 12:36:22 +0200

    Update index.js

--- modules/home/index.js
+++ modules/home/index.js
@@ -39,7 +39,7 @@ define([ 'require','libbf'], function ( require, libbf ) {
                 //     }
                 // }
 
-                        BFInstallation.search({ subjectId: tagId, relType: REL_TYPE_INSTALLATION}).then(function (installations) { //timestamp: (new Date()).toISOString() }
+                        BFInstallation.search({ subjectId: personId, relType: REL_TYPE_INSTALLATION}).then(function (installations) { //timestamp: (new Date()).toISOString() }
                             function install() {
                                 // BFInstallation.search({ objectId: personId, subjectId: tagId, relType: REL_TYPE_INSTALLATION, timestamp: (new Date()).toISOString() }).then(function () {
                                     BFInstallation.persist({
