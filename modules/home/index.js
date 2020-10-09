13d376f49a93b1a66848a15ded00ecb172d17e03
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-10-09 14:57:04 +0200

    Update index.js

--- modules/home/index.js
+++ modules/home/index.js
@@ -172,6 +172,9 @@ define([ 'require','libbf'], function ( require, libbf ) {
 
         $ctrl.scanStart = function () {
 //             window.alert('version 1.1');
+            if(state===false){
+                return;
+            }
             $scope.state = true;    
             const reader = new NDEFReader(); 
             // { signal: controller.signal }  
