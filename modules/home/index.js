0b8879ea345a5a4774b9a47e97f324b7e80633ec
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-10-09 12:00:11 +0200

    Update index.js

--- modules/home/index.js
+++ modules/home/index.js
@@ -250,9 +250,7 @@ define([ 'require','libbf'], function ( require, libbf ) {
                                 const today=(new Date()).toISOString();
                                 InstallationsService.search({objectId:subjects[0].id,relType: 'is-installed-at',timestamp:(new Date()).toISOString()}).then(function(installations){//relType: 'is-installed-at',timestamp:today,subjectId: tag
                                     if(installations!=0){
-                                    installations.forEach(function(thing){
-                                    window.alert(thing.name);
-                                    })
+                                        console.log(installtions);
                                     // inst.endVt=(new Date()).toISOString();
                                     // InstallationsService.persist(inst).then(function resolve(){
                                     //     window.alert('Unpairing process was succesfull!');
