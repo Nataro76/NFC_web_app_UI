2d0f273097c092dcd13cb1e1f6293564420a8154
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-10-09 12:03:17 +0200

    Update index.js

--- modules/home/index.js
+++ modules/home/index.js
@@ -249,7 +249,7 @@ define([ 'require','libbf'], function ( require, libbf ) {
                                 window.alert('You are unpairing the "'+ you+'" tag.');
                                 const today=(new Date()).toISOString();
                                 InstallationsService.search({objectId:subjects[0].id,relType: 'is-installed-at',timestamp:(new Date()).toISOString()}).then(function(installations){//relType: 'is-installed-at',timestamp:today,subjectId: tag
-                                    if(installations!=0){
+                                    if(installations.length!=0){
                                         console.log(installations);
                                     // inst.endVt=(new Date()).toISOString();
                                     // InstallationsService.persist(inst).then(function resolve(){
