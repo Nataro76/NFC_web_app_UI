9e2b96e91d38eb8847f64e87d72b061afde4ab4a
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-10-09 09:47:45 +0200

    h

--- modules/home/index.js
+++ modules/home/index.js
@@ -121,7 +121,7 @@ define([ 'require','libbf'], function ( require, libbf ) {
             window.addEventListener('error', errorFun());
             console.log('Beta version 2.35/troubleshooting');
             $scope.success=false;
-            $unpaired=!$unpaired;
+            $scope.unpaired=!$scope.unpaired;
 
 
 
@@ -226,7 +226,7 @@ define([ 'require','libbf'], function ( require, libbf ) {
 
         };
         $ctrl.unpairing = function (){
-            $scope.unpaired;
+            $scope.unpaired=!$scope.unpaired;
             var reader = new NDEFReader();
             reader.scan();
                     reader.onreading =({message,serialNumber}) =>{
